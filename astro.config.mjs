03f180fac1bb116353d686d9ef47c765629db03d
nkaewam <tontan2545@gmail.com>
2026-03-28 17:14:16 +0700

    feat: fonts and images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "astro/config"
+import { defineConfig, fontProviders } from "astro/config"
 import react from "@astrojs/react"
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare"
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,8 +12,34 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Anakotmai",
+      cssVariable: "--font-anakotmai",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/anakotmai/Anakotmai-medium.woff2"],
+            weight: "normal",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/anakotmai/Anakotmai-light.woff2"],
+            weight: "light",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/anakotmai/Anakotmai-bold.woff2"],
+            weight: "bold",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
   integrations: [react()],
   adapter: cloudflare({
-    imageService: "compile"
+    imageService: "compile",
   }),
 })
