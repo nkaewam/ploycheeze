ff1a729e91f4f7a35b9903d857c6ccfe5e95bdbe
nkaewam <tontan2545@gmail.com>
2026-03-28 15:28:31 +0700

    feat: cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,16 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [react()],
+  adapter: cloudflare({
+    imageService: "compile"
+  }),
 })
